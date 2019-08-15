c2c1c0ab0e7bae713c87977305a92f086ef20cec
Stephen Parker <stephenparkhum@outlook.com>
2019-08-15 09:14:10 -0700

    Fixing a bug in the template

--- src/app.js
+++ src/app.js
@@ -5,15 +5,15 @@ const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 
-const app = express()''
+const app = express();
 
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');
